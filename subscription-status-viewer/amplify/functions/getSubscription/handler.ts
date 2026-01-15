ba6c99def24365fd96c86e205655cc863d155b5a
Batoul <batoulhammoud157@gmail.com>
2026-01-14 22:27:44 -0500

    Finished the authentication requirement and added a basic dashboard

--- subscription-status-viewer/amplify/functions/getSubscription/handler.ts
+++ subscription-status-viewer/amplify/functions/getSubscription/handler.ts
@@ -1,2 +1,6 @@
 
+import type { AppSyncResolverHandler, AppSyncIdentityCognito } from 'aws-lambda';
 
+export const handler: AppSyncResolverHandler<any, any> = async (event) => {
+  //TODO: Implement the logic to get subscription status based on the event arguments
+}
