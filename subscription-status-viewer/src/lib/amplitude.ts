6692659e84d4da40841dcae17e0e881fc1839f0e
Batoul <batoulhammoud157@gmail.com>
2026-01-17 21:48:47 -0500

    chore: moved amplitude public api key to be an env variable

--- subscription-status-viewer/src/lib/amplitude.ts
+++ subscription-status-viewer/src/lib/amplitude.ts
@@ -1,7 +1,9 @@
 import * as amplitude from "@amplitude/analytics-browser";
 
-// Initialize with your Amplitude API key
-amplitude.init("3a56c73c56d454e242ea6f8e9f98704e");
+
+const AMPLITUDE_KEY = import.meta.env.VITE_AMPLITUDE_API_KEY;
+
+amplitude.init(AMPLITUDE_KEY);
 
 export const logEvent = (eventName: string, eventProperties?: Record<string, any>) => {
   amplitude.track(eventName, eventProperties);
