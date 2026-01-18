bb9269c348210291d6c45ed0ce22138e8b828ac9
Batoul <batoulhammoud157@gmail.com>
2026-01-17 20:23:46 -0500

    Updated amplitude api key

--- subscription-status-viewer/src/lib/amplitude.ts
+++ subscription-status-viewer/src/lib/amplitude.ts
@@ -1,7 +1,7 @@
 import * as amplitude from "@amplitude/analytics-browser";
 
 // Initialize with your Amplitude API key
-amplitude.init("YOUR_AMPLITUDE_API_KEY");
+amplitude.init("3a56c73c56d454e242ea6f8e9f98704e");
 
 export const logEvent = (eventName: string, eventProperties?: Record<string, any>) => {
   amplitude.track(eventName, eventProperties);
